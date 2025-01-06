d485c116eb5e6bb8cd84d8a48759b900a17976aa
Benjamin Chen <bc880321@gmail.com>
2025-01-06 07:05:43 -0800

    styling

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -13,7 +13,7 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="dashboard bg-gray-100 min-h-screen" style={{marginTop: "20vh"}}>
+    <div className="dashboard bg-gray-100 min-h-screen" style={{marginTop: "13vh"}}>
       <div className="dashboard container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-4 mt-4">
         <aside className="dashboard-sidebar col-span-1 bg-white p-4 rounded-lg shadow">
           <CustomerList onSelectCustomer={handleSelectCustomer} />
